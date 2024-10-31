5f8f02fa7752b11a52afa4f05edef2a88bc89e06
Morphos-afk <jbalbsan@myuax.com>
2024-10-31 18:17:00 +0100

    Fin de los apartados 1.1 1.2 1.3 Fatla ej 1.4 creando un nuevo archico cpp que muestre el uso de varios tipos de datos

--- Declaracion_variables/main.cpp
+++ Declaracion_variables/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <string>
+#include <typeinfo>
 
     int current_year = 2024; // Variable global
     int mayoria_edad = 18;  // Variable global para mayoría de edad
-
 
     void mostrar_year_nacimiento(int edad)
     // Variable local por que solo es accesible desde donde se ya se ha definido edad
@@ -12,6 +12,15 @@
     std::cout << "Naciste el año: " << year_nacimiento;
     }
 
+    void verificar_mayoria_edad(int edad)
+    {
+        if (edad >= mayoria_edad){
+            std::cout << "Eres mayor de edad. \n ";
+        } else
+        {
+            std::cout << " Eres menor de edad. \n ";
+        }
+    }
 int main()
 {
 
@@ -34,5 +43,13 @@ int main()
 
     std::cout << "Hola  " << nombre << "! :) Tienes " << edad << " años. \n\n";
         mostrar_year_nacimiento(edad);
+        verificar_mayoria_edad(edad);
+
+        //Mostrar tipos de variables
+                std::cout << "Para este ejercicio se han usado los tipos de datos:  ";
+                std::cout << "Tipo de 'entero': " << typeid(current_year).name() << "\n";
+
+
     return 0;
+
 }
