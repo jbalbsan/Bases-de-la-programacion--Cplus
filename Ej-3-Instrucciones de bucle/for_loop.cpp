e98e9e123bbf04750eb0267f66c3e1b61b41235c
Morphos-afk <jbalbsan@myuax.com>
2024-11-01 19:33:13 +0100

    Creación ejercicio 3

---
+++ Ej-3-Instrucciones de bucle/for_loop.cpp
@@ -0,0 +1,10 @@
+// for_loop.cpp
+#include <iostream>
+#include "for_loop.h"
+
+void imprimirNumerosFor() {
+    for (int i = 1; i <= 1; ++i) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+}
---
+++ Ej-3-Instrucciones de bucle/while_loop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "while_loop.h"
+
+void imprimirTablaMultiplicar(int numero) { // Usamos el número pasado como parámetro
+    int contador = 1;
+
+    while (contador <= 10) {
+        std::cout << numero << " x " << contador << " = " << (numero * contador) << "\n"; // Imprimir la tabla
+        contador++; // Incrementar el contador +1 por cada uso
+    }
+}
+
+
